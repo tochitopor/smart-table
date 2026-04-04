b1545f3b5256465c6f38504f51c2ccb7c1b69da6
tochitopor <abichaika@yandex.ru>
2026-04-04 20:05:56 +0300

    шаг 2 ботва с номерами

--- src/main.js
+++ src/main.js
@@ -47,10 +47,12 @@ async function render(action) {
     // result = applyFiltering(result, state, action); // фильтрация
     // result = applySorting(result, state, action); // сортировка
     // result = applyPagination(result, state, action); // использование
+    query = applyPagination(query, state, action); // обновляем query
 
-    const { total, items } = await api.getRecords(query);
+    const { total, items } = await api.getRecords(query); // запрашиваем данные с собранными параметрами
 
-    sampleTable.render(items)
+    updatePagination(total, query); // перерисовываем пагинатор
+    sampleTable.render(items);
 }
 
 const sampleTable = initTable({
@@ -61,7 +63,7 @@ const sampleTable = initTable({
 }, render);
 
 // инициализация
-const applyPagination = initPagination(
+const {applyPagination, updatePagination} = initPagination(
     sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
     (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
         const input = el.querySelector('input');
